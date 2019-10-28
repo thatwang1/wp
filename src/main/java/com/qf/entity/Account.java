7d0301998b878ac6b7aaa3adb06c1a9ad220a765
thatwang1 <wp676559071@163.com>
2019-10-28 19:48:03 +0800

    Initial commit

---
+++ src/main/java/com/qf/entity/Account.java
@@ -0,0 +1,49 @@
+package com.qf.entity;
+
+import java.io.Serializable;
+
+/**
+ * @author lxq
+ * @date 2019-09-06 18:14
+ */
+public class Account  implements Serializable {
+
+
+    private Integer id;
+    private String name;
+    private Double money;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getMoney() {
+        return money;
+    }
+
+    public void setMoney(Double money) {
+        this.money = money;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", money=" + money +
+                '}';
+    }
+}
